1cdd9ac7d6b5092248ef3c18333201207bc3ad3f
HOA <hoa@lifestyle-design.jp>
2017-08-27 11:42:59 +0900

    Creating Tab Page in the footer
    In the footer, create Tab Page to navigate to all of the page of the
    system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,12 +1,8 @@
+import { RecipesPage } from './../recipes/recipes';
+import { RecipePage } from './../recipe/recipe';
+import { ShopingListPage } from './../shoping-list/shoping-list';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the TabsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,6 +11,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TabsPage {
 
+  slPage = ShopingListPage;
+  recipesPage = RecipesPage;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
