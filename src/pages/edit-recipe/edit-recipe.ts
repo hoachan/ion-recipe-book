82dfeaca00535a38141eb13d21077dfc4a41aa5a
HOA <hoa@lifestyle-design.jp>
2017-08-30 17:04:53 +0900

    Adding Ingredients to Form
    How to use FormGroup, FormControl, FormArray
    and how to learn using View way into the app with
    AlertController, ToastController, ActionSheetController

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { 
+        IonicPage, 
+        NavController, 
+        NavParams, 
+        ActionSheetController, 
+        AlertController, 
+        ToastController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -13,7 +19,11 @@ export class EditRecipePage implements OnInit {
   selectOptions = ['easy', 'medium', 'hard'];
   recipeForm : FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+             public navParams: NavParams,
+             public actionSheetController : ActionSheetController,
+             private alertCtrl : AlertController,
+             private toastCtrl : ToastController) {
   }
 
   ionViewDidLoad() {
@@ -29,11 +39,85 @@ export class EditRecipePage implements OnInit {
     this.recipeForm = new FormGroup({
       'title' : new FormControl(null, Validators.required),
       'description' : new FormControl(null, Validators.required),
-      'difficulty' : new FormControl('medium', Validators.required)
+      'difficulty' : new FormControl('medium', Validators.required),
+      'ingredients' : new FormArray([])
     });
   }
 
   onSubmit(){
     console.log(this.recipeForm);
+  }
+
+  onManageIngredients(){
+    const actionSheet = this.actionSheetController.create({
+      'title' : 'What do u want to do?',
+      buttons : [
+        {
+          text : 'Add Ingredient',
+          handler: () => {
+            this.createNewIngredientAlert().present();
+          }
+        },
+        {
+          text : 'Remove all Ingredients ',
+          role : 'destructive',
+          handler : () => {
+            const fArray : FormArray = <FormArray>this.recipeForm.get('ingredients');
+            const len = fArray.length;
+            if (len > 0) {
+              for (let i = len -1; i >= 0; i--){
+                fArray.removeAt(i);
+              }
+            }
+            const toast =  this.toastCtrl.create({
+              message : 'Ingredient was deleted !',
+              duration : 1500,
+              position :'bottom'
+            });
+            toast.present();
+          }
+        },
+        {
+          text : 'Cancel',
+          role : 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  private createNewIngredientAlert(){
+    return this.alertCtrl.create({
+      title : 'Add Ingredient',
+      inputs : [
+        {
+          name : 'name',
+          placeholder : 'Name'
+        }
+      ],
+      buttons : [
+        {
+          text : 'Cancel',
+          role : 'cancel'
+        },
+        {
+          text : 'Add',
+          handler : data => {
+            if (data.name.trim() == '' || data.name == null) {
+              const toast =  this.toastCtrl.create({
+                message : 'Please enter a valid value !',
+                duration : 2000,
+                position :'bottom'
+              });
+              toast.present();
+              return ;
+            }
+
+            (<FormArray>this.recipeForm.get('ingredients')).
+              push(new FormControl(data.name, Validators.required));
+          }
+        }
+      ]
+    });
   }
 }
