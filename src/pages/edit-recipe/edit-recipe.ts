abd04c74e4c1df810fb28fffb34ac0865ca6a4ec
HOA <hoa@lifestyle-design.jp>
2017-08-30 13:33:08 +0900

    Creating Form Controller
    How to use FormGroup, FormControl, Validators
    Note :
    
        this.recipeForm = new FormGroup({
          'title' : new FormControl(null, Validators.required),
          'description' : new FormControl(null, Validators.required),
          'difficulty' : new FormControl('medium', Validators.required)
        });

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
@@ -6,7 +7,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-edit-recipe',
   templateUrl: 'edit-recipe.html',
 })
-export class EditRecipePage {
+export class EditRecipePage implements OnInit {
+
+  mode = 'New';
+  selectOptions = ['easy', 'medium', 'hard'];
+  recipeForm : FormGroup;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -15,4 +20,20 @@ export class EditRecipePage {
     console.log('ionViewDidLoad EditRecipePage');
   }
 
+  ngOnInit(){
+    this.mode = this.navParams.get('mode');
+    this.initializeForm();
+  }
+
+  private initializeForm(){
+    this.recipeForm = new FormGroup({
+      'title' : new FormControl(null, Validators.required),
+      'description' : new FormControl(null, Validators.required),
+      'difficulty' : new FormControl('medium', Validators.required)
+    });
+  }
+
+  onSubmit(){
+    console.log(this.recipeForm);
+  }
 }
