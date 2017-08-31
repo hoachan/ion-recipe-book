8afea4e35e17f253c85c5be86c7af7dcdb37fbe5
HOA <hoa@lifestyle-design.jp>
2017-08-31 18:45:28 +0900

    how to use authenticate for sign in and sign up in progress
    in progress, we can log in and log out app with
        firebase.auth().onAuthStateChanged(user => {
          if (user) {
            this.isAuthenticated = true;
            this.rootPage = TabsPage;
          } else {
            this.isAuthenticated = false;
            this.rootPage = SigninPage;
          }
        });

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
+import { AuthService } from './../services/auth';
 import { SignupPage } from './../pages/signup/signup';
 import { SigninPage } from './../pages/signin/signin';
 import { TabsPage } from './../pages/tabs/tabs';
 import { Component, ViewChild } from '@angular/core';
-import { Platform, NavController, MenuController } from 'ionic-angular';
+import { Platform, NavController, MenuController, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -12,18 +13,33 @@ import firebase from 'firebase';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  tabsPage = TabsPage;
+  rootPage: any = TabsPage;
   signinPage = SigninPage;
   signupPage = SignupPage;
+  isAuthenticated = false;
 
   /**Using ViewChild instead of DI navController */
   @ViewChild('nav') nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl : MenuController) {
+  constructor(
+              platform: Platform, statusBar: StatusBar, 
+              splashScreen: SplashScreen, private menuCtrl : MenuController,
+              private alertCtrl : AlertController, private authService : AuthService
+            ) {
 
     firebase.initializeApp({
       apiKey: "apiKey of firebase",
       authDomain: "domain auth",
+    });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.isAuthenticated = true;
+        this.rootPage = TabsPage;
+      } else {
+        this.isAuthenticated = false;
+        this.rootPage = SigninPage;
+      }
     });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -39,7 +55,8 @@ export class MyApp {
   }
 
   onLogout() {
-
+    this.authService.logout();
+    this.menuCtrl.close();
   }
 }
 
