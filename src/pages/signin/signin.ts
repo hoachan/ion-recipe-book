2b4d57cd2e882ef80ee45534dfed65689609eeae
HOA <hoa@lifestyle-design.jp>
2017-08-31 18:21:45 +0900

    add LoadingController and AlertController
    How to using Loading Controller and Alert Controller when sign in and
    sign up

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,7 @@
+import { AuthService } from './../../services/auth';
 import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -9,7 +10,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SigninPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+                public navCtrl: NavController, 
+                public navParams: NavParams,
+                private authService : AuthService,
+                private alertCtrl : AlertController,
+                private loadingCtrl : LoadingController
+              ) {
   }
 
   ionViewDidLoad() {
@@ -18,5 +25,24 @@ export class SigninPage {
 
   onSignin(form : NgForm){
     console.log(form);
+    const loading = this.loadingCtrl.create({
+      content : 'Singin you in....'
+    });
+    loading.present();
+    this.authService.signin(form.value.email, form.value.password)
+      .then(data => {
+        console.log(data);
+        loading.dismiss();
+      })
+      .catch(error => {
+        loading.dismiss();
+        const alert = this.alertCtrl.create({
+          title : 'Signin Failded',
+          message : error.message,
+          buttons : ['Ok']
+        });
+        alert.present();
+        console.log(error);
+      });
   }
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,7 +1,7 @@
 import { AuthService } from './../../services/auth';
 import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -13,7 +13,9 @@ export class SignupPage {
   constructor(
               public navCtrl: NavController, 
               public navParams: NavParams,
-              private authService : AuthService  
+              private authService : AuthService,
+              private loadingCtrl : LoadingController,
+              private alertCtrl : AlertController
             ) {}
 
   ionViewDidLoad() {
@@ -22,6 +24,21 @@ export class SignupPage {
 
   onSignup(form : NgForm){
     console.log(form);
-    this.authService.signup(form.value.email, form.value.password).then(data => console.log(data)).catch(error => console.log(error));
+    const loading = this.loadingCtrl.create({
+      content : 'i am loading...'
+    });
+    this.authService.signup(form.value.email, form.value.password).then(data => {
+      console.log(data);
+      loading.dismiss();
+    }).catch(error => {
+      console.log(error);
+      loading.dismiss();
+      const alert = this.alertCtrl.create({
+        title : 'Singup faild',
+        message : error.message,
+        buttons : ['Ok']
+      });
+      alert.present();
+    });
   }
 }
